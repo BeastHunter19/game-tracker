07cda7116cbff16418579f770371a96dd718c668
Francesco Coppola <ciccio-peppe@live.com>
2023-03-11 14:43:52 +0100

    clarity refactors in navbar

--- client/src/components/NavbarMain.vue
+++ client/src/components/NavbarMain.vue
@@ -16,13 +16,12 @@ export default {
 <template>
     <header>
         <nav class="navbar navbar-expand-lg fixed-top navbar-dark shadow">
-
-            <!--Navigation-->
             <div class="d-flex justify-content-between container-fluid">
-                <!--Brand-->
+                <!-- Brand -->
                 <a class="navbar-brand ms-3" href="#">Game Tracker</a>
 
-                <div class="collapse navbar-collapse multi-collapse w-auto" id="navbarTogglerLeft">
+                <!-- Navigation -->
+                <div class="collapse navbar-collapse multi-collapse w-auto" id="navigation-panel">
                     <ul class="navbar-nav nav-pills me-auto mb-2 mb-lg-0">
                         <li v-for="(page, index) in appPages" :key="index" class="nav-item mx-2">
                             <RouterLink class="nav-link" active-class="active shadow" :to="page.path">
@@ -32,6 +31,7 @@ export default {
                     </ul>
                 </div>
 
+                <!-- Search bar -->
                 <form role="search">
                     <div class="input-group">
                         <input class="form-control form-control-lg" type="search" placeholder="Search" aria-label="Search">
@@ -41,14 +41,16 @@ export default {
                     </div>
                 </form>
 
-                <span class="collapse navbar-collapse multi-collapse w-auto" id="navbarTogglerRight">
+                <!-- Login buttons -->
+                <span class="collapse navbar-collapse multi-collapse" id="login-panel">
                     <button type="button" class="btn btn-primary shadow me-2">Login</button>
                     <button type="button" class="btn btn-primary shadow">Sign in</button>
                 </span>
             </div>
 
+            <!-- Toggler -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse"
-                aria-controls="navbarTogglerLeft navbarTogglerLeft" aria-expanded="false" aria-label="Toggle navigation">
+                aria-controls="navigation-panel login-panel" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
         </nav>
