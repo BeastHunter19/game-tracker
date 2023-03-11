4010d0a19e460e72920dc15ac7dc958115bcd132
Francesco Coppola <ciccio-peppe@live.com>
2023-03-11 16:43:33 +0100

    Started fixing responsive behavior

--- client/src/components/NavbarMain.vue
+++ client/src/components/NavbarMain.vue
@@ -16,43 +16,50 @@ export default {
 <template>
     <header>
         <nav class="navbar navbar-expand-lg fixed-top navbar-dark shadow">
-            <div class="d-flex justify-content-between container-fluid">
-                <!-- Brand -->
-                <a class="navbar-brand ms-3" href="#">Game Tracker</a>
+            <div class="container-fluid text-center">
+                <div class="row flex-grow-1">
+                    <!-- Main section -->
+                    <div class="collapse navbar-collapse multi-collapse col-lg-4" id="navigation-panel">
+                        <!-- Brand -->
+                        <a class="navbar-brand ms-3" href="#">Game Tracker</a>
 
-                <!-- Navigation -->
-                <div class="collapse navbar-collapse multi-collapse w-auto" id="navigation-panel">
-                    <ul class="navbar-nav nav-pills me-auto mb-2 mb-lg-0">
-                        <li v-for="(page, index) in appPages" :key="index" class="nav-item mx-2">
-                            <RouterLink class="nav-link" active-class="active shadow" :to="page.path">
-                                {{ page.name }}
-                            </RouterLink>
-                        </li>
-                    </ul>
+                        <!-- Navigation -->
+                        <ul class="navbar-nav nav-pills me-auto mb-2 mb-lg-0">
+                            <li v-for="(page, index) in appPages" :key="index" class="nav-item mx-2">
+                                <RouterLink class="nav-link" active-class="active aria-current shadow" :to="page.path">
+                                    {{ page.name }}
+                                </RouterLink>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <!-- Search bar -->
+                    <div class="col-lg-4 me-4 me-lg-0">
+                        <form role="search">
+                            <div class="input-group">
+                                <input class="form-control form-control-lg" type="search" placeholder="Search"
+                                    aria-label="Search">
+                                <button class="btn btn-primary" type="submit">
+                                    <i class="bi bi-search"></i>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+
+                    <!-- Login buttons -->
+                    <div class="collapse navbar-collapse multi-collapse col-lg-4 justify-content-end" id="login-panel">
+                        <button type="button" class="btn btn-primary shadow me-2">Login</button>
+                        <button type="button" class="btn btn-primary shadow">Sign in</button>
+                    </div>
                 </div>
 
-                <!-- Search bar -->
-                <form role="search">
-                    <div class="input-group">
-                        <input class="form-control form-control-lg" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-primary" type="submit">
-                            <i class="bi bi-search"></i>
-                        </button>
-                    </div>
-                </form>
-
-                <!-- Login buttons -->
-                <span class="collapse navbar-collapse multi-collapse" id="login-panel">
-                    <button type="button" class="btn btn-primary shadow me-2">Login</button>
-                    <button type="button" class="btn btn-primary shadow">Sign in</button>
-                </span>
+                <!-- Toggler -->
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse"
+                    aria-controls="navigation-panel login-panel" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
             </div>
 
-            <!-- Toggler -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse"
-                aria-controls="navigation-panel login-panel" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
         </nav>
     </header>
 </template>
