7a1825fc7af8c9cf7ab9d21147770578284a18ce
Francesco Coppola <ciccio-peppe@live.com>
2023-03-11 14:04:38 +0100

    Started adding support for multiple collapsible sections in navbar

--- client/src/components/NavbarMain.vue
+++ client/src/components/NavbarMain.vue
@@ -15,18 +15,24 @@ export default {
 
 <template>
     <header>
-        <nav class="navbar navbar-expand-lg fixed-top navbar-dark shadow justify-content-between">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Game Tracker</a>
-                <div class="collapse navbar-collapse" id="navbarToggler">
+        <nav class="navbar navbar-expand-lg fixed-top navbar-dark shadow">
+
+            <!--Navigation-->
+            <div class="d-flex justify-content-between container-fluid">
+                <!--Brand-->
+                <a class="navbar-brand ms-3" href="#">Game Tracker</a>
+
+                <div class="collapse navbar-collapse multi-collapse w-auto" id="navbarTogglerLeft">
                     <ul class="navbar-nav nav-pills me-auto mb-2 mb-lg-0">
-                        <li v-for="(page, index) in appPages" :key="index" class="nav-item">
-                            <RouterLink class="nav-link" active-class="active" :to="page.path">{{ page.name }}</RouterLink>
+                        <li v-for="(page, index) in appPages" :key="index" class="nav-item mx-2">
+                            <RouterLink class="nav-link" active-class="active shadow" :to="page.path">
+                                {{ page.name }}
+                            </RouterLink>
                         </li>
                     </ul>
                 </div>
 
-                <form class="d-flex mx-auto" role="search">
+                <form role="search">
                     <div class="input-group">
                         <input class="form-control form-control-lg" type="search" placeholder="Search" aria-label="Search">
                         <button class="btn btn-primary" type="submit">
@@ -35,40 +41,45 @@ export default {
                     </div>
                 </form>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler"
-                    aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                <span class="collapse navbar-collapse multi-collapse w-auto" id="navbarTogglerRight">
+                    <button type="button" class="btn btn-primary shadow me-2">Login</button>
+                    <button type="button" class="btn btn-primary shadow">Sign in</button>
+                </span>
             </div>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse"
+                aria-controls="navbarTogglerLeft navbarTogglerLeft" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
         </nav>
     </header>
 </template>
 
-<style>
+<style scoped>
 .btn-primary {
-    --bs-btn-bg: var(--gt-color-main);
-    --bs-btn-border-color: var(--gt-color-main);
-    --bs-btn-hover-bg: var(--gt-color-main-hover);
-    --bs-btn-hover-border-color: var(--gt-color-main);
-    --bs-btn-active-bg: var(--gt-color-main-active);
-    --bs-btn-active-border-color: var(--gt-color-main-active);
+    --bs-btn-bg: var(--gt-color-secondary);
+    --bs-btn-border-color: var(--gt-color-secondary);
+    --bs-btn-hover-bg: var(--gt-color-secondary-hover);
+    --bs-btn-hover-border-color: var(--gt-color-secondary);
+    --bs-btn-active-bg: var(--gt-color-secondary-active);
+    --bs-btn-active-border-color: var(--gt-color-secondary-active);
 }
 
 .navbar {
-    background-color: var(--bs-indigo);
+    background-color: var(--gt-color-main);
     --bs-navbar-nav-link-padding-x: 1rem;
     text-transform: capitalize;
 }
 
 .nav-pills {
-    --bs-nav-pills-link-active-bg: var(--gt-color-main);
+    --bs-nav-pills-link-active-bg: var(--gt-color-secondary);
 }
 
 .navbar .form-control {
-    border-color: var(--gt-color-main);
+    border-color: var(--gt-color-secondary);
 }
 
 .navbar .form-control:focus {
-    box-shadow: 0 0 0 0.25rem var(--gt-color-main-transparent);
+    box-shadow: 0 0 0 0.25rem var(--gt-color-secondary-transparent);
 }
 </style>
