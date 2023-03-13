e7be8ad32bc5974ed1ede99dbbf4d5dc98512e16
Francesco Coppola <ciccio-peppe@live.com>
2023-03-13 22:56:47 +0100

    Added game card component

---
+++ client/src/components/GameCard.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+    props: {
+        gameInfo: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card m-4 text-bg-light" style="width: 18rem">
+        <img :src="gameInfo.image" class="card-img-top" alt="Cover image for the game" />
+        <div class="card-body">
+            <h5 class="card-title">{{ gameInfo.title + ' (' + gameInfo.release + ')' }}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{{ gameInfo.developer }}</h6>
+
+            <div class="d-flex flex-wrap mb-1">
+                <span
+                    v-for="(genre, index) in gameInfo.genres"
+                    :key="index"
+                    class="badge rounded-pill text-bg-secondary m-1"
+                    >{{ genre }}</span
+                >
+            </div>
+            <div class="d-flex flex-wrap">
+                <span
+                    v-for="(platform, index) in gameInfo.platforms"
+                    :key="index"
+                    class="badge rounded-pill text-bg-primary m-1"
+                    >{{ platform }}</span
+                >
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    border: none;
+}
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,5 +1,25 @@
-<script setup></script>
+<script>
+import GameCard from '@/components/GameCard.vue'
+
+export default {
+    data() {
+        return {
+            testInfo: {
+                title: 'Bloodborne',
+                release: '2015',
+                developer: 'From Software',
+                genres: ['Adventure', 'Role-playing (RPG)', 'Action', 'Souls-like'],
+                platforms: ['PlayStation 4', 'PC (magari)'],
+                image: 'https://assets.reedpopcdn.com/-1616688899670.jpg/BROK/thumbnail/1600x900/quality/100/-1616688899670.jpg'
+            }
+        }
+    },
+    components: { GameCard }
+}
+</script>
 
 <template>
-    <main></main>
+    <main>
+        <GameCard :gameInfo="testInfo"></GameCard>
+    </main>
 </template>
