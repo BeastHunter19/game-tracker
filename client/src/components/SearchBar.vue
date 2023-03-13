d02ac60d338acd03d7a8d99962b001d4fc3247e5
Francesco Coppola <ciccio-peppe@live.com>
2023-03-13 19:04:38 +0100

    Added separate SearchBar component

---
+++ client/src/components/SearchBar.vue
@@ -0,0 +1,25 @@
+<script>
+export default {}
+</script>
+
+<template>
+    <form role="search" class="col">
+        <div class="input-group shadow">
+            <input
+                class="form-control form-control-lg"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+            />
+            <button class="btn btn-primary" type="submit">
+                <i class="bi bi-search"></i>
+            </button>
+        </div>
+    </form>
+</template>
+
+<style scoped>
+form {
+    max-width: 600px;
+}
+</style>
