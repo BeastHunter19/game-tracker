f981f5326fadcfa3f74585a20ea60bde439416ea
Francesco Coppola <ciccio-peppe@live.com>
2023-03-14 21:24:21 +0100

    Added helmet to express server

--- app/app.js
+++ app/app.js
@@ -2,14 +2,20 @@ const express = require('express')
 require('dotenv').config()
 const { logger } = require('./logger.js')
 const morgan = require('morgan')
+const helmet = require('helmet')
 
 const port = process.env.PORT
 const app = express()
 
+app.use(helmet())
 app.use(morgan('dev'))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.json('Hello, world')
+})
 
 app.listen(port, () => {
     logger.info('Server listening at http://localhost:' + port)
