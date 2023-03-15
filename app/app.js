cfdd779bd8e46aa0d8799ad12109bfa270d0a5e7
Francesco Coppola <ciccio-peppe@live.com>
2023-03-15 19:07:42 +0100

    Added cookie parser middleware in express app

--- app/app.js
+++ app/app.js
@@ -3,14 +3,16 @@ const config = require('./config')
 const { logger } = require('./logger.js')
 const pinoHTTP = require('pino-http')
 const helmet = require('helmet')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 
 app.use(pinoHTTP({ logger }))
 app.use(helmet())
 
-app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.json('Hello, world')
