de26209c457496b5cd91db0835988d66e8530a7b
Francesco Coppola <ciccio-peppe@live.com>
2023-03-15 22:35:11 +0100

    Added create function to User model

--- app/models/User.js
+++ app/models/User.js
@@ -1,5 +1,6 @@
 const db = require('../utils/db')
 const { logger } = require('../logger')
+const bcrypt = require('bcryptjs')
 
 const User = {}
 
@@ -10,5 +11,17 @@ User.getAll = async () => {
     } catch (err) {
         logger.error(err, 'Could not retrieve users from database')
         throw err
+    }
+}
+
+User.create = async (name, email, password) => {
+    try {
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const newUser = await db.query(
+            `INSERT INTO users(id, name, email, password) VALUES(UUID_TO_BIN(UUID()), ?, ?, ?)`,
+            [name, email, hashedPassword]
+        )
+    } catch (err) {
+        logger.error(err, 'Could not add new user to database')
     }
 }
