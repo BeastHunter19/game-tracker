ace23828dfcd0e2e4f24994ee4f08b437cb96bc6
Francesco Coppola <ciccio-peppe@live.com>
2023-03-18 19:17:14 +0100

    Added passport configuration

---
+++ app/utils/passport.js
@@ -0,0 +1,53 @@
+const passport = require('passport')
+const bcrypt = require('bcryptjs')
+const LocalStrategy = require('passport-local').Strategy
+const JwtStrategy = require('passport-jwt').Strategy
+const { ExtractJwt } = require('passport-jwt')
+const User = require('../models/User')
+require('dotenv').config()
+
+passport.use(
+    new LocalStrategy(
+        {
+            usernameField: 'email',
+            passwordField: 'password'
+        },
+        function (username, password, cb) {
+            return User.getByEmail(username)
+                .then((existingUser) => {
+                    if (!existingUser) {
+                        return cb(null, false, { message: 'Incorrect email' })
+                    }
+                    return bcrypt.compare(password, existingUser.password).then((result) => {
+                        if (!result) {
+                            return cb(null, false, { message: 'Incorrect password' })
+                        }
+                        return cb(null, user)
+                    })
+                })
+                .catch((err) => {
+                    return cb(err)
+                })
+        }
+    )
+)
+
+passport.use(
+    new JwtStrategy(
+        {
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: process.env.ACCESS_JWT_SECRET
+        },
+        function (jwt_payload, cb) {
+            return User.getByID(jwt_payload.id)
+                .then((existingUser) => {
+                    return cb(null, user || false)
+                })
+                .catch((err) => {
+                    return cb(err, false)
+                })
+        }
+    )
+)
+
+module.exports = passport
