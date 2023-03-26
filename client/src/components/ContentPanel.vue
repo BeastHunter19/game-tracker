d3594c5b1364d4bbb1bbfe2c73f856921e91b867
Francesco Coppola <ciccio-peppe@live.com>
2023-03-27 00:14:17 +0200

    Reduced ContentPanel margins on bigger screens

--- client/src/components/ContentPanel.vue
+++ client/src/components/ContentPanel.vue
@@ -3,7 +3,7 @@ export default {}
 </script>
 
 <template>
-    <div class="panel px-2 px-md-5 py-5 my-5 mx-2 mx-md-5 text-center rounded-5">
+    <div class="panel px-2 px-md-5 py-5 my-4 mx-2 mx-md-4 text-center rounded-5">
         <slot></slot>
     </div>
 </template>
