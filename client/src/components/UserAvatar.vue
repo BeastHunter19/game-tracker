ee57ca1954e1c2b225a27d5d78b499e8c99269e0
Francesco Coppola <ciccio-peppe@live.com>
2023-03-29 14:16:42 +0200

    Made avatar size adjustable

--- client/src/components/UserAvatar.vue
+++ client/src/components/UserAvatar.vue
@@ -3,6 +3,12 @@ import { mapState } from 'pinia'
 import { useUserStore } from '@/stores/user'
 
 export default {
+    props: {
+        size: {
+            type: String,
+            default: '45px'
+        }
+    },
     computed: {
         ...mapState(useUserStore, ['user', 'loggedIn']),
         userInitials() {
@@ -11,23 +17,27 @@ export default {
             }
             const names = this.user.name.split(/\s+/, 2)
             return names[0][0]?.toUpperCase() + names[1][0]?.toUpperCase()
+        },
+        sizeStyle() {
+            return `width: ${this.size}; height: ${this.size};`
         }
     }
 }
 </script>
 
 <template>
-    <span class="d-inline-block avatar rounded-circle shadow fw-bold text-center">{{
-        userInitials
-    }}</span>
+    <span
+        class="d-inline-block avatar rounded-circle shadow fw-bold text-center d-flex align-items-center justify-content-center"
+        :style="sizeStyle"
+    >
+        <span>{{ userInitials }}</span>
+    </span>
 </template>
 
 <style scoped>
 .avatar {
     padding: 12px;
     background-color: var(--gt-color-main-lighter);
-    width: 45px;
-    height: 45px;
     line-height: 21px;
     margin-top: -5px;
     margin-bottom: -5px;
