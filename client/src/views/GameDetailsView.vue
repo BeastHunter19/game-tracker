7a0255357e67672250b88f4c7af0eb8242b1c33d
Francesco Coppola <ciccio-peppe@live.com>
2023-03-30 20:01:21 +0200

    Added image carousel to Game Details view

--- client/src/views/GameDetailsView.vue
+++ client/src/views/GameDetailsView.vue
@@ -2,10 +2,116 @@
 import ContentPanel from '@/components/ContentPanel.vue'
 
 export default {
-    components: { ContentPanel }
+    components: { ContentPanel },
+    data() {
+        return {
+            gameInfo: {
+                id: 1234,
+                title: 'Bloodborne',
+                release: '2015',
+                developer: 'From Software',
+                genres: ['Adventure', 'Role-playing (RPG)', 'Action', 'Souls-like'],
+                platforms: ['PlayStation 4', 'PC (magari)'],
+                cover: 'https://assets.reedpopcdn.com/-1616688899670.jpg/BROK/thumbnail/1600x900/quality/100/-1616688899670.jpg',
+                images: [
+                    'https://www.lorebloodborne.it/wp-content/uploads/2021/09/Lady-Maria.jpg',
+                    'https://assets.reedpopcdn.com/news-videogiochi-bloodborne-pc-tutti-rumor-sono-100-percent-fake-modder-lance-mcdonald-1618329215390.jpg/BROK/thumbnail/1200x630/news-videogiochi-bloodborne-pc-tutti-rumor-sono-100-percent-fake-modder-lance-mcdonald-1618329215390.jpg',
+                    'https://images.everyeye.it/img-articoli/bloodborne-25340.jpg'
+                ],
+                added: true
+            }
+        }
+    }
 }
 </script>
 
 <template>
-    <ContentPanel class="my-4 mx-2 mx-md-4"> </ContentPanel>
+    <main class="w-100 mw-100">
+        <div class="container-fluid p-0 m-0 mt-4">
+            <div class="row g-0 justify-content-around row-cols-1 mw-100 ms-lg-4">
+                <div class="col mw-100 mb-4">
+                    <div
+                        class="gallery-panel text-center rounded-5 mx-2 mx-md-4 ms-lg-0 me-lg-4 p-0 h-100 overflow-hidden"
+                    >
+                        <div id="gameImagesCarousel" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-indicators">
+                                <button
+                                    v-for="(image, index) in gameInfo.images"
+                                    :key="index"
+                                    type="button"
+                                    data-bs-target="#gameImagesCarousel"
+                                    :data-bs-slide-to="index"
+                                    :class="{ active: index === 0 }"
+                                    :aria-current="index === 0"
+                                    :aria-label="`Image ${index + 1}`"
+                                ></button>
+                            </div>
+                            <div class="carousel-inner">
+                                <div
+                                    v-for="(image, index) in gameInfo.images"
+                                    :key="index"
+                                    class="carousel-item custom-item"
+                                    :class="{ active: index === 0 }"
+                                    data-bs-interval="5000"
+                                >
+                                    <img
+                                        :src="image"
+                                        class="d-block w-100 custom-img"
+                                        :alt="`${gameInfo.title} image ${index + 1}`"
+                                    />
+                                </div>
+                            </div>
+                            <button
+                                class="carousel-control-prev"
+                                type="button"
+                                data-bs-target="#gameImagesCarousel"
+                                data-bs-slide="prev"
+                            >
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button
+                                class="carousel-control-next"
+                                type="button"
+                                data-bs-target="#gameImagesCarousel"
+                                data-bs-slide="next"
+                            >
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row g-0 justify-content-around row-cols-1 row-cols-lg-2 mw-100 ms-lg-4">
+                <div class="col mw-100 mb-4">
+                    <ContentPanel
+                        class="mx-2 mx-md-4 ms-lg-0 me-lg-4 py-lg-4 px-lg-2 h-100 d-flex justify-content-center align-items-center gap-4"
+                    >
+                    </ContentPanel>
+                </div>
+                <div class="col mw-100">
+                    <ContentPanel
+                        class="mb-4 mx-2 mx-md-4 ms-lg-0 me-lg-4 py-lg-4 px-lg-2 d-flex justify-content-center"
+                    >
+                    </ContentPanel>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
+
+<style scoped>
+.gallery-panel {
+    background-color: rgba(0, 0, 0, 0.3);
+}
+
+.custom-item {
+    height: 50vh;
+}
+
+.custom-img {
+    height: 100%;
+    object-fit: contain;
+}
+</style>
