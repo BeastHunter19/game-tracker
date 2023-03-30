a4a5f6d8be32146d1cc78fc81a9c6c797f53f331
Francesco Coppola <ciccio-peppe@live.com>
2023-03-30 22:18:49 +0200

    Added more informations in Game Details

--- client/src/views/GameDetailsView.vue
+++ client/src/views/GameDetailsView.vue
@@ -8,8 +8,9 @@ export default {
             gameInfo: {
                 id: 1234,
                 title: 'Bloodborne',
-                release: '2015',
+                release: 'March 24, 2015',
                 developer: 'From Software',
+                description: `Bloodborne è un videogioco souls-like dark fantasy sviluppato dalla software house giapponese FromSoftware, con la collaborazione di SCE Japan Studio e pubblicato da Sony Computer Entertainment in esclusiva per PlayStation 4. Diretto da Hidetaka Miyazaki, già ideatore di Demon's Souls e di Dark Souls.`,
                 genres: ['Adventure', 'Role-playing (RPG)', 'Action', 'Souls-like'],
                 platforms: ['PlayStation 4', 'PC (magari)'],
                 cover: 'https://assets.reedpopcdn.com/-1616688899670.jpg/BROK/thumbnail/1600x900/quality/100/-1616688899670.jpg',
@@ -18,6 +19,7 @@ export default {
                     'https://assets.reedpopcdn.com/news-videogiochi-bloodborne-pc-tutti-rumor-sono-100-percent-fake-modder-lance-mcdonald-1618329215390.jpg/BROK/thumbnail/1200x630/news-videogiochi-bloodborne-pc-tutti-rumor-sono-100-percent-fake-modder-lance-mcdonald-1618329215390.jpg',
                     'https://images.everyeye.it/img-articoli/bloodborne-25340.jpg'
                 ],
+                rating: 99,
                 added: true
             }
         }
@@ -83,17 +85,46 @@ export default {
                     </div>
                 </div>
             </div>
-            <div class="row g-0 justify-content-around row-cols-1 row-cols-lg-2 mw-100 ms-lg-4">
-                <div class="col mw-100 mb-4">
-                    <ContentPanel
-                        class="mx-2 mx-md-4 ms-lg-0 me-lg-4 py-lg-4 px-lg-2 h-100 d-flex justify-content-center align-items-center gap-4"
-                    >
+            <div
+                class="row g-0 justify-content-around row-cols-1 row-cols-lg-2 mw-100 ms-lg-4 mb-4"
+            >
+                <div class="col mw-100">
+                    <ContentPanel class="mx-2 mx-md-4 ms-lg-0 me-lg-4 py-lg-4 px-4 h-100 gap-4">
+                        <h1 class="text-start">{{ gameInfo.title }}</h1>
+                        <h2 class="text-start">{{ gameInfo.release }}</h2>
+                        <h3 class="text-start">{{ gameInfo.developer }}</h3>
+                        <p class="text-start fs-5">{{ gameInfo.description }}</p>
                     </ContentPanel>
                 </div>
                 <div class="col mw-100">
-                    <ContentPanel
-                        class="mb-4 mx-2 mx-md-4 ms-lg-0 me-lg-4 py-lg-4 px-lg-2 d-flex justify-content-center"
-                    >
+                    <ContentPanel class="mb-4 mx-2 mx-md-4 ms-lg-0 me-lg-4 py-lg-4 px-lg-2 h-100">
+                        <div class="row mb-4">
+                            <div class="col-6 fs-5">
+                                Critics aggregate rating: {{ gameInfo.rating }}
+                            </div>
+                        </div>
+                        <div class="row mb-4">
+                            <span class="col-3 fs-5">Categories:</span>
+                            <div class="col-9 d-flex flex-wrap align-items-center gap-2">
+                                <span
+                                    v-for="(genre, index) in gameInfo.genres"
+                                    :key="index"
+                                    class="badge rounded-pill text-bg-secondary"
+                                    >{{ genre }}</span
+                                >
+                            </div>
+                        </div>
+                        <div class="row mb-4">
+                            <span class="col-3 fs-5">Platforms:</span>
+                            <div class="col-9 d-flex flex-wrap align-items-center gap-2">
+                                <span
+                                    v-for="(platform, index) in gameInfo.platforms"
+                                    :key="index"
+                                    class="badge rounded-pill text-bg-primary"
+                                    >{{ platform }}</span
+                                >
+                            </div>
+                        </div>
                     </ContentPanel>
                 </div>
             </div>
