2f74a37377ecd4072fae7c88894e63d09629b382
Francesco Coppola <ciccio-peppe@live.com>
2023-04-12 20:49:34 +0200

    Removed unused test array

--- client/src/views/ProfileView.vue
+++ client/src/views/ProfileView.vue
@@ -9,19 +9,6 @@ import { mapStores, mapActions } from 'pinia'
 
 export default {
     components: { MainGamesPanel, UserAvatar, ContentPanel },
-    data() {
-        return {
-            testArray: Array(20).fill({
-                title: 'Bloodborne',
-                release: '2015',
-                developer: 'From Software',
-                genres: ['Adventure', 'Role-playing (RPG)', 'Action', 'Souls-like'],
-                platforms: ['PlayStation 4', 'PC (magari)'],
-                image: 'https://assets.reedpopcdn.com/-1616688899670.jpg/BROK/thumbnail/1600x900/quality/100/-1616688899670.jpg',
-                added: true
-            })
-        }
-    },
     computed: {
         ...mapStores(useGamesStore, useUserStore),
         ownedGames() {
