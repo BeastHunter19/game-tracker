2c2cdf5a17afd0755c650a3777bd33cda289bed8
Francesco Coppola <ciccio-peppe@live.com>
2023-04-14 11:39:45 +0200

    Removed useless actions

--- client/src/views/WatchlistView.vue
+++ client/src/views/WatchlistView.vue
@@ -1,15 +1,12 @@
 <script>
 import GamesPanelExpanded from '@/components/GamesPanelExpanded.vue'
 import { useGamesStore } from '@/stores/games'
-import { mapState, mapActions } from 'pinia'
+import { mapState } from 'pinia'
 
 export default {
     components: { GamesPanelExpanded },
     computed: {
         ...mapState(useGamesStore, ['watchlist'])
-    },
-    methods: {
-        ...mapActions(useGamesStore, ['fetchAllWatchlist'])
     }
 }
 </script>
