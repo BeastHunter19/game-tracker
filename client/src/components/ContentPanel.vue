2776cfec2222504ae2f5c32a5631c69600447a03
Francesco Coppola <ciccio-peppe@live.com>
2023-04-16 19:51:52 +0200

    Removed semi transparent background panels

--- client/src/components/ContentPanel.vue
+++ client/src/components/ContentPanel.vue
@@ -3,13 +3,7 @@ export default {}
 </script>
 
 <template>
-    <div class="panel px-2 px-md-5 py-5 text-center rounded-5">
+    <div class="panel px-2 px-md-3 text-center">
         <slot></slot>
     </div>
 </template>
-
-<style scoped>
-.panel {
-    background-color: rgba(255, 255, 255, 0.3);
-}
-</style>
