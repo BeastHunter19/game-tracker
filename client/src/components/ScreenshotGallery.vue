1d9aa7c068ba9443c4543204de7581963030d56e
Francesco Coppola <ciccio-peppe@live.com>
2023-04-16 21:32:46 +0200

    Moved screenshot gallery to separate component

---
+++ client/src/components/ScreenshotGallery.vue
@@ -0,0 +1,83 @@
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            default: 'Game'
+        },
+        images: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<template>
+    <div
+        class="gallery-panel text-center rounded-5 mx-2 mx-md-4 ms-lg-0 me-lg-4 p-0 h-100 overflow-hidden"
+    >
+        <div id="gameImagesCarousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button
+                    v-for="(image, index) in images"
+                    :key="index"
+                    type="button"
+                    data-bs-target="#gameImagesCarousel"
+                    :data-bs-slide-to="index"
+                    :class="{ active: index === 0 }"
+                    :aria-current="index === 0"
+                    :aria-label="`Image ${index + 1}`"
+                ></button>
+            </div>
+            <div class="carousel-inner">
+                <div
+                    v-for="(image, index) in images"
+                    :key="index"
+                    class="carousel-item custom-item"
+                    :class="{ active: index === 0 }"
+                    data-bs-interval="5000"
+                >
+                    <img
+                        :src="image"
+                        class="d-block w-100 custom-img"
+                        :alt="`${title} image ${index + 1}`"
+                    />
+                </div>
+            </div>
+            <button
+                class="carousel-control-prev"
+                type="button"
+                data-bs-target="#gameImagesCarousel"
+                data-bs-slide="prev"
+            >
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+                class="carousel-control-next"
+                type="button"
+                data-bs-target="#gameImagesCarousel"
+                data-bs-slide="next"
+            >
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.gallery-panel {
+    background-color: rgba(0, 0, 0, 0.3);
+}
+
+.custom-item {
+    height: 70vh;
+}
+
+.custom-img {
+    height: 100%;
+    object-fit: contain;
+}
+</style>
--- client/src/views/GameDetailsView.vue
+++ client/src/views/GameDetailsView.vue
@@ -3,6 +3,7 @@ import ContentPanel from '@/components/ContentPanel.vue'
 import GamesPanel from '@/components/GamesPanel.vue'
 import AddButtons from '@/components/AddButtons.vue'
 import LoadingSpinner from '@/components/LoadingSpinner.vue'
+import ScreenshotGallery from '@/components/ScreenshotGallery.vue'
 import { useNotificationsStore } from '@/stores/notifications'
 import { useUserStore } from '@/stores/user'
 import { useGamesStore } from '@/stores/games'
@@ -11,7 +12,14 @@ import CompletedButton from '@/components/CompletedButton.vue'
 
 export default {
     name: 'GameDetailsView',
-    components: { ContentPanel, GamesPanel, AddButtons, LoadingSpinner, CompletedButton },
+    components: {
+        ContentPanel,
+        GamesPanel,
+        AddButtons,
+        LoadingSpinner,
+        CompletedButton,
+        ScreenshotGallery
+    },
     data() {
         return {
             gameInfo: {},
@@ -83,57 +91,10 @@ export default {
         <div v-else class="container-fluid p-0 m-0 mt-4">
             <div class="row g-0 justify-content-around row-cols-1 mw-100 ms-lg-4">
                 <div class="col mw-100 mb-4">
-                    <div
-                        class="gallery-panel text-center rounded-5 mx-2 mx-md-4 ms-lg-0 me-lg-4 p-0 h-100 overflow-hidden"
-                    >
-                        <div id="gameImagesCarousel" class="carousel slide" data-bs-ride="carousel">
-                            <div class="carousel-indicators">
-                                <button
-                                    v-for="(image, index) in gameInfo.images"
-                                    :key="index"
-                                    type="button"
-                                    data-bs-target="#gameImagesCarousel"
-                                    :data-bs-slide-to="index"
-                                    :class="{ active: index === 0 }"
-                                    :aria-current="index === 0"
-                                    :aria-label="`Image ${index + 1}`"
-                                ></button>
-                            </div>
-                            <div class="carousel-inner">
-                                <div
-                                    v-for="(image, index) in gameInfo.images"
-                                    :key="index"
-                                    class="carousel-item custom-item"
-                                    :class="{ active: index === 0 }"
-                                    data-bs-interval="5000"
-                                >
-                                    <img
-                                        :src="image"
-                                        class="d-block w-100 custom-img"
-                                        :alt="`${gameInfo.title} image ${index + 1}`"
-                                    />
-                                </div>
-                            </div>
-                            <button
-                                class="carousel-control-prev"
-                                type="button"
-                                data-bs-target="#gameImagesCarousel"
-                                data-bs-slide="prev"
-                            >
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
-                            </button>
-                            <button
-                                class="carousel-control-next"
-                                type="button"
-                                data-bs-target="#gameImagesCarousel"
-                                data-bs-slide="next"
-                            >
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
-                            </button>
-                        </div>
-                    </div>
+                    <ScreenshotGallery
+                        :title="gameInfo.title"
+                        :images="gameInfo.images"
+                    ></ScreenshotGallery>
                 </div>
             </div>
             <div class="row g-0 justify-content-around row-cols-1 row-cols-lg-2 mw-100 ms-lg-4">
@@ -235,18 +196,3 @@ export default {
         </div>
     </main>
 </template>
-
-<style scoped>
-.gallery-panel {
-    background-color: rgba(0, 0, 0, 0.3);
-}
-
-.custom-item {
-    height: 70vh;
-}
-
-.custom-img {
-    height: 100%;
-    object-fit: contain;
-}
-</style>
